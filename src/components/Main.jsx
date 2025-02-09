f4562c57c0fa92936411328ab05833739d05eb54
Dhairya-unfiltered <shahdhairya245@gmail.com>
2025-02-09 13:54:53 +0530

    Add files via upload

---
+++ src/components/Main.jsx
@@ -0,0 +1,32 @@
+import React from 'react'
+import videoBg from '../assets/swiftshops_bg.mp4'
+import sparkle from '../assets/sparkle.png'
+import award from '../assets/feather1.png'
+import Navbar from './Navbar'
+
+
+const Main = () => {
+  return (
+    <div className='main'>
+  <video src={videoBg} autoPlay loop muted className='video-bg' />
+  <Navbar />
+  <div className='content'>
+    
+    <div className='inside text-center px-5 md:px-64'>
+    <div className='flex -mb-5 justify-center'>
+        <img src={award} className='award' />
+    </div>
+    <h1 className='text-white text-3xl md:text-7xl font-bold mb-5'>Hate Waiting in Queues?</h1>
+    <p className='text-gray-300 text-lg md:text-2xl'>Skip the hassle. Go Swift Shopping!
+    </p>
+    <button type="button" class="text-white bg-pink-500 font-semibold rounded-full text-lg md:text-2xl px-5  text-center mt-5"><img src={sparkle} className='sparkleimg' />Join the Waitlist</button>
+    </div>
+  </div>
+  
+
+</div>
+
+  )
+}
+
+export default Main
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,70 @@
+import React, { useState } from "react";
+import logo from '../assets/ss_logo.png'
+
+const Navbar = () => {
+  let [isOpen, setIsOpen] = useState(false);
+  return (
+    <nav className="bg-transparent text-white w-full">
+      <div className="md:px-5 py-4 px-3 md:flex justify-between items-center">
+        <div className="flex text-3xl cursor-pointer items-center gap-2">
+            <img src={logo} className="logo rounded-md" />
+          <span className="font-bold">SwiftShops</span>
+        </div>
+        <div
+          onClick={() => setIsOpen(!isOpen)}
+          className="w-10 h-10 absolute right-8 top-6 cursor-pointer md:hidden transition-transform duration-300 ease-in-out"
+        >
+          {isOpen ? (
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth={1.5}
+              stroke="currentColor"
+              className="transform rotate-180"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M6 18 18 6M6 6l12 12"
+              />
+            </svg>
+          ) : (
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth={1.5}
+              stroke="currentColor"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M3.75 6.75h16.5M3.75 12h16.5M12 17.25h8.25"
+              />
+            </svg>
+          )}
+        </div>
+        <ul
+          className={`md:flex text-xl pl-9 md:pl-0 md:items-center md:pb-0 pb-12 absolute md:static bg-transparent md:z-auto z-[-1] left-0 w-full md:w-auto md:pl-0 transition-all duration-500 ease-in ${
+            isOpen ? 'top-20 opacity-100' : 'top-[-200px] md:opacity-100 opacity-0'
+          }`}
+        >
+          <li className="font-semibold my-7 md:my-0 md:ml-8">
+            <a href="https://www.linkedin.com/in/shahdhairya245" target="_blank" rel="noopener noreferrer">Waitlist</a>
+          </li>
+          <button
+            type="button"
+            className="bg-white text-black font-semibold text-xl md:ml-8 rounded-full md:static px-5 py-2.5"
+          >
+            <a href="https://tally.so/r/n0Zzp6" target="_blank" rel="noopener noreferrer">
+              Contact Us
+            </a>
+          </button>
+        </ul>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;

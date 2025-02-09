b13a6dbe009340e7e1482995cd1b9c1071c6bf77
Dhairya-unfiltered <shahdhairya245@gmail.com>
2025-02-09 23:15:48 +0530

    Add files via upload

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,32 +1,48 @@
-import React from 'react'
-import videoBg from '../assets/swiftshops_bg.mp4'
-import sparkle from '../assets/sparkle.png'
-import award from '../assets/feather1.png'
-import Navbar from './Navbar'
-
+import React, { useState } from 'react';
+import videoBg from '../assets/swiftshops_bg.mp4';
+import sparkle from '../assets/sparkle.png';
+import award from '../assets/feather1.png';
+import Navbar from './Navbar';
 
 const Main = () => {
+  const [videoLoaded, setVideoLoaded] = useState(false);
+
+  const handleVideoLoad = () => {
+    setVideoLoaded(true);
+  };
+
   return (
-    <div className='main'>
-  <video src={videoBg} autoPlay loop muted className='video-bg' />
-  <Navbar />
-  <div className='content'>
-    
-    <div className='inside text-center px-5 md:px-64'>
-    <div className='flex -mb-5 justify-center'>
-        <img src={award} className='award' />
+    <div className={`main ${videoLoaded ? '' : 'bg-black'}`}>
+      <video 
+        src={videoBg} 
+        autoPlay 
+        loop 
+        muted 
+        className="video-bg" 
+        onLoadedData={handleVideoLoad} 
+      />
+      <Navbar />
+      <div className="content">
+        <div className="inside text-center px-5 md:px-64">
+          <div className="flex -mb-5 justify-center">
+            <img src={award} className="award" />
+          </div>
+          <h1 className="text-white text-3xl md:text-7xl font-bold mb-5">Hate Waiting in Queues?</h1>
+          <p className="text-gray-300 text-lg md:text-2xl">
+            Swift Shops is sculpting the new era of hassle free shopping--where waiting is a thing of past.
+          </p>
+          <a href="https://tally.so/r/n0Zzp6">
+            <button 
+              type="button" 
+              className="text-white bg-pink-500 font-semibold rounded-full text-lg md:text-2xl px-5 text-center mt-5"
+            >
+              <img src={sparkle} className="sparkleimg" />Join the Waitlist
+            </button>
+          </a>
+        </div>
+      </div>
     </div>
-    <h1 className='text-white text-3xl md:text-7xl font-bold mb-5'>Hate Waiting in Queues?</h1>
-    <p className='text-gray-300 text-lg md:text-2xl'>Swift Shops is sculpting the new era of hassle free shopping--where waiting is a thing of past.
-    </p>
-    <a href="https://tally.so/r/n0Zzp6"><button type="button" class="text-white bg-pink-500 font-semibold rounded-full text-lg md:text-2xl px-5  text-center mt-5"><img src={sparkle} className='sparkleimg' />Join the Waitlist</button></a>
-    </div>
-  </div>
-  
+  );
+};
 
-</div>
-
-  )
-}
-
-export default Main
+export default Main;

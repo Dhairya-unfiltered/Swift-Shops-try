c8e9e337b037de6e4dfe9205da4a22c9ae1e9112
Dhairya-unfiltered <shahdhairya245@gmail.com>
2025-02-09 14:53:10 +0530

    Add files via upload

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -17,7 +17,7 @@ const Main = () => {
         <img src={award} className='award' />
     </div>
     <h1 className='text-white text-3xl md:text-7xl font-bold mb-5'>Hate Waiting in Queues?</h1>
-    <p className='text-gray-300 text-lg md:text-2xl'>Skip the hassle. Go Swift Shopping!
+    <p className='text-gray-300 text-lg md:text-2xl'>Swift Shops is sculpting the new era of hassle free shopping--where waiting is a thing of past.
     </p>
     <button type="button" class="text-white bg-pink-500 font-semibold rounded-full text-lg md:text-2xl px-5  text-center mt-5"><img src={sparkle} className='sparkleimg' />Join the Waitlist</button>
     </div>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import logo from '../assets/ss_logo.png'
+import logo from '../assets/newl.png'
 
 const Navbar = () => {
   let [isOpen, setIsOpen] = useState(false);
